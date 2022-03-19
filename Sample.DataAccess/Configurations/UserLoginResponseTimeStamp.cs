225450803f464bac74d58b4f52f9bafb76b334dd
yusuf uğurlu <yusuf_ugurlu2015@outlook.com>
2022-03-19 19:11:40 +0300

    add column primary key in UserLoginResponseTimeStamp

--- Sample.DataAccess/Configurations/UserLoginResponseTimeStamp.cs
+++ Sample.DataAccess/Configurations/UserLoginResponseTimeStamp.cs
@@ -8,6 +8,7 @@ namespace Sample.DataAccess.Configurations
 {
     public class UserLoginResponseTimeStamp
     {
+        public int Id { get; set; }
         public long TimeStamp { get; set; }
         public DateTime ResponseStart { get; set; }
         public DateTime ResponseEnd { get; set; }
